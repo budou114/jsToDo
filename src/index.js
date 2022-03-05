30d083da79322148a82482249b7716ac9700915a
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-05 13:46:17 +0900

    テキストボックスの値を取得し、初期化する

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const onClickAdd = () => {
+    // テキストボックスの値を取得し、初期化する
+    const inputText = document.getElementById('add-text').value;
+    document.getElementById('add-text').value = '';
+    alert(inputText);
+};
+
+document.getElementById('add-button').addEventListener('click', () => onClickAdd());
